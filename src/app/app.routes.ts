81992aa20fddcb7e70030d2a20f734198c32fd84
Anil <anil0x80@users.noreply.github.com>
2025-07-17 17:12:51 +0300

    add register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { LoginPage } from './login-page/login-page';
+import { RegisterPage } from './register-page/register-page';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path:'sign-in',
         component:LoginPage
+    },
+    {
+        path:'sign-up',
+        component:RegisterPage
     }
 ];
