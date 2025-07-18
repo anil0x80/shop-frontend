caf0c73c5cea17ddedf39d49b158b584714d0791
Anil <anil0x80@users.noreply.github.com>
2025-07-18 11:10:28 +0300

    improve register page

--- src/app/register-page/register-page.ts
+++ src/app/register-page/register-page.ts
@@ -1,21 +1,84 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
-import {ReactiveFormsModule, Validators} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {FormGroup, FormControl, MinLengthValidator} from '@angular/forms';
+import {ReactiveFormsModule, Validators,} from '@angular/forms';
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+
+enum PasswordError {
+  Strength = 'passwordStrength',
+  Mismatch = 'passwordMismatch',
+}
 
 @Component({
   selector: 'app-register-page',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css'
 })
+
 export class RegisterPage {
     registerForm = new FormGroup({
     username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
-  });
+    password: new FormControl('', 
+        [Validators.required,
+           this.passwordStrengthValidator]),
+    repeated_password: new FormControl('', 
+        [Validators.required]),
+  }, { validators: this.passwordMatchValidator });
 
-  onSignup()
-  {
-    alert("username: " + this.registerForm.value.username + "  " + "password: " + this.registerForm.value.password)
+  // validator for password match
+  passwordMatchValidator(group: AbstractControl): ValidationErrors | null {
+    const password = group.value.password;
+    const repeatedPassword = group.value.repeated_password;
+    
+    if (password !== repeatedPassword) {
+        return { [PasswordError.Mismatch]: true };
+    }
+    return null;
+  }
+
+  // validator for password strength
+  passwordStrengthValidator (control:AbstractControl) : ValidationErrors | null {
+      const value = control.value;
+
+      if (!value) {
+          return null;
+      }
+
+      const hasUpperCase = /[A-Z]+/.test(value);
+
+      const hasLowerCase = /[a-z]+/.test(value);
+
+      const hasNumeric = /[0-9]+/.test(value);
+
+      const isLengthGood = value.length >= 6;
+
+      const passwordValid = hasUpperCase && hasLowerCase && hasNumeric && isLengthGood;
+
+      return !passwordValid ? {[PasswordError.Strength]: true}: null;
+  }
+
+  hasPasswordMismatchError(){
+    return this.registerForm.hasError(PasswordError.Mismatch) && this.repeated_password.touched;
+  }
+
+  hasPasswordWeakError() {
+    return this.password.hasError(PasswordError.Strength) && this.password.touched;
+  }
+
+  hasAnyPasswordError() {
+    return this.hasPasswordWeakError() || this.hasPasswordMismatchError();
+  }
+
+  get password() {
+      return this.registerForm.controls.password;
+  }
+
+  get repeated_password() {
+      return this.registerForm.controls.repeated_password;
+  }
+
+  onSignup() {
+    alert("username: " + this.registerForm.value.username + "  " + "password: " + this.registerForm.value.password);
   }
 }
