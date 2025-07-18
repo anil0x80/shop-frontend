05b3bfb84368aac81a399b46facb544f2424eb3b
BARIŞ KILIÇ <bariskilic2003@gmail.com>
2025-07-18 09:40:26 +0300

    changed icons with mat-icons

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -2,11 +2,12 @@ import { CurrencyPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import {FormControl, ReactiveFormsModule,FormGroup,FormBuilder} from '@angular/forms';
 import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-header',
-  imports: [CurrencyPipe,ReactiveFormsModule],
+  imports: [CurrencyPipe, ReactiveFormsModule, MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
