15edb3cd1740c741d605e2a59d10f35c0f827e7e
BARIŞ KILIÇ <bariskilic2003@gmail.com>
2025-07-21 10:32:58 +0300

    product page init

--- src/app/product-page/product-page.ts
+++ src/app/product-page/product-page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,inject, OnInit} from '@angular/core';
+import { ProductService} from '../services/product-service'; 
+import { Product } from '../models/product.model';
+import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-page',
@@ -6,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './product-page.html',
   styleUrl: './product-page.css'
 })
-export class ProductPage {
+export class ProductPage implements OnInit{
+  private router = inject(Router)
+  productId: string | null = null;
+  product: Product | undefined;
+  private productService = inject(ProductService);
+  private route = inject(ActivatedRoute);
 
+  ngOnInit(): void {
+    this.productId = this.route.snapshot.paramMap.get('id');
+    this.productId = this.route.snapshot.paramMap.get('id');
+    if (this.productId) {
+      this.product = this.productService.getProductById(this.productId); 
+    } else {
+      console.error('Product ID is null');
+    }
+  }
 }
