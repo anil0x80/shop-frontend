79f1749733a713dec9d154c689f4eb612f911f09
BARIŞ KILIÇ <bariskilic2003@gmail.com>
2025-07-24 11:47:27 +0300

    initial home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ProductCard } from "../product-card/product-card";
+import { ProductService } from '../services/product-service';
+import { CategoryService } from '../services/category-service';
+import { Category } from '../models/category.model';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,19 @@ import { ProductCard } from "../product-card/product-card";
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit{
+  private productService = inject(ProductService);
+  private categoryService = inject(CategoryService)
 
+  categories:Category[] | undefined;
+  productsByCategory= new Map<Category,Product[]>;
+
+  ngOnInit(): void {
+    this.categories = this.categoryService.getCategories();
+    for (let index = 0; index < this.categories.length; index++) {
+      const category = this.categories[index];
+      const products = this.productService.getProductsByCategory(category);
+      this.productsByCategory.set(category,products);
+    }
+  }
 }
